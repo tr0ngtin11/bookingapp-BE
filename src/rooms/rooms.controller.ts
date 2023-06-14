54f1b43eb2e7c10857733664074e1d4f87f8c359
Tr0ngTin <71576097+tr0ngtin11@users.noreply.github.com>
2023-06-14 10:53:31 +0700

    fix: add header and Pipe for each method

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -3,71 +3,89 @@ import {
   Get,
   Post,
   Body,
-  Patch,
+  Put,
   Param,
   Delete,
+  Header,
+  Res,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
-
+import { Response, Request } from 'express';
 @Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
   @Post()
-  async create(@Body() createRoomDto: CreateRoomDto) {
+  async create(@Body() createRoomDto: CreateRoomDto, @Res() res: Response) {
     const rooms = await this.roomsService.create(createRoomDto);
     if (!rooms) return new Error('Create room failed');
-
-    return {
+    res.header('X-Total-Count', '1');
+    res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+    return res.json({
       message: 'Create room successfully',
-    };
+    });
   }
 
   @Get()
-  async findAll() {
+  // @Header('Access-Control-Expose-Headers', 'X-Total-Count')
+  async findAll(@Res() res: Response) {
     try {
       const rooms = await this.roomsService.findAll();
       if (!rooms) return new Error('Get rooms failed');
-      return rooms;
+      res.header('X-Total-Count', rooms.length.toString());
+      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+      return res.json(rooms);
     } catch (error) {
       console.log(error);
     }
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  async findOne(@Param('id', ParseIntPipe) id: string, @Res() res: Response) {
     try {
       const room = await this.roomsService.findOne(+id);
       if (!room) return new Error('Room not found');
-      return room;
+      res.header('X-Total-Count', '1');
+      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+      return res.json(room);
     } catch (error) {
       console.log(error);
     }
   }
 
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateRoomDto: UpdateRoomDto,
+    @Res() res: Response,
+  ) {
     try {
       const room = await this.roomsService.update(+id, updateRoomDto);
+      console.log('aaa', room);
       if (!room) return new Error('Update room failed');
-      return {
+      res.header('X-Total-Count', '1');
+      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+      return res.json({
         message: 'Update room successfully',
-      };
+      });
     } catch (error) {
       console.log(error);
     }
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  async remove(@Param('id', ParseIntPipe) id: string, @Res() res: Response) {
     try {
       const room = await this.roomsService.remove(+id);
       if (!room) return new Error('Delete room failed');
-      return {
+      res.header('X-Total-Count', '1');
+      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+      return res.json({
         message: 'Delete room successfully',
-      };
+      });
     } catch (error) {
       console.log(error);
     }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,78 +3,91 @@ import {
   Get,
   Post,
   Body,
-  Patch,
+  Put,
   Param,
   Delete,
   ParseIntPipe,
+  Res,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { Response } from 'express';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
     try {
       const { confirmPassword, ...detailUser } = createUserDto;
       const user = this.usersService.create(detailUser);
       if (!user) return new Error('Create user failed');
-
-      return {
+      res.header('X-Total-Count', '1');
+      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+      return res.json({
         message: 'Create user successfully',
-      };
+      });
     } catch (error) {
       console.log(error);
     }
   }
 
   @Get()
-  async findAll() {
+  async findAll(@Res() res: Response) {
     try {
       const users = await this.usersService.findAll();
       if (!users) return new Error('Get users failed');
-      return users;
+      res.header('X-Total-Count', users.length.toString());
+      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+      return res.json(users);
     } catch (error) {
       console.log(error);
     }
   }
 
   @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number) {
+  async findOne(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
     try {
       const user = await this.usersService.findOne(id);
       if (!user) return new Error('User not found');
-      return user;
+      res.header('X-Total-Count', '1');
+      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+      return res.json(user);
     } catch (error) {
       console.log(error);
     }
   }
 
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Res() res: Response,
+  ) {
     try {
-      const res = this.usersService.update(+id, updateUserDto);
-      if (!res) return new Error('Update user failed');
-
-      return {
+      const response = this.usersService.update(+id, updateUserDto);
+      if (!response) return new Error('Update user failed');
+      res.header('X-Total-Count', '1');
+      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+      return res.json({
         message: 'Update user successfully',
-      };
+      });
     } catch (error) {
       console.log(error);
     }
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  async remove(@Param('id', ParseIntPipe) id: string, @Res() res: Response) {
     try {
-      const res = this.usersService.remove(+id);
+      const response = this.usersService.remove(+id);
       if (!res) return new Error('Delete user failed');
-      return {
+      res.header('X-Total-Count', '1');
+      res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+      return res.json({
         message: 'Delete user successfully',
-      };
+      });
     } catch (error) {
       console.log(error);
     }
